143ceab312583ffb02a39bb3304c8f48d8cc37b7
Untitled <littleluluno01@163.com>
2017-03-26 07:05:07 +0800

    update server list

--- webRoot/js/cust_core/config.js
+++ webRoot/js/cust_core/config.js
@@ -2,9 +2,9 @@ var DEBUG = true;
 var __YH_SRV_LIST__ =
 [
   "http://tourguide.tpddns.cn:8888/",
-  "http://10.30.161.87/"
+  "http://120.25.213.220:8888/"
 ];
-var __YH_SRV__ = __YH_SRV_LIST__[0];
+var __YH_SRV__ = __YH_SRV_LIST__[1];
 var __YH_IMG_URL__ = __YH_SRV__ + "static/";
 var __USER_INFO_UPDATE__ = __YH_SRV__ + "API/UserInfoUpdate/";
 var __USER_POS_UPLOAD__ = __YH_SRV__ + "API/UpLoadPath/";
